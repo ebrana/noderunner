bc15ca50a1845d9ab0eff7257d57ae3763b611f8
Jan Dvořák <dvorak@ebrana.cz>
2015-06-25 08:25:53 +0200

    disabled forever service watchfiles by default, tuning of default params, bug fix in immediate module

--- bin/initScript-watch.sh
+++ bin/initScript-watch.sh
@@ -9,7 +9,7 @@ case "$1" in
   cd ..
   mkdir -p /var/log/noderunner
   forever stop noderunner.js > /dev/null 2>&1
-  forever start -a -p . --minUptime 1000 --spinSleepTime 100 -e /var/log/noderunner/error.log -l /var/log/noderunner/forever.log --pidFile ./forever.pid noderunner.js
+  forever start -a -p . --minUptime 1000 --spinSleepTime 100 -e /var/log/noderunner/error.log -l /var/log/noderunner/forever.log --pidFile ./forever.pid -w --watchIgnore "logs/*" noderunner.js
   ;;
 stop)
   exec forever stop noderunner.js
