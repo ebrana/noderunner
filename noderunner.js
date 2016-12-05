e42857b410bb2469de9e599f185b63382ea98a6d
Jan Dvorak <dvorak@ebrana.cz>
2016-12-05 12:35:25 +0100

    improved logging, auto mongo reconnect when topology desctruction detected, changed immediate free threads management mechanism

--- noderunner.js
+++ noderunner.js
@@ -15,33 +15,64 @@ var logger = require('./lib/logger')(nconf.get('logLevel'));
 var immediate, planned, history, watchdog, gui;
 
 // Mongo connection check
-(function tryMongoConnection() {
-    MongoClient.connect(nconf.get('mongoDSN'), function(err, db){
-        logger.info('Connected to mongo queuerunner DB');
+var mongoTimeout;
+function tryMongoConnection() {
+    var options = {
+        server: { reconnectTries: 100, reconnectInterval: 1000, socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+        replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
+    };
+    MongoClient.connect(nconf.get('mongoDSN'), options, function(err, db){
         if (err) {
             logger.error('Mongo connection error, try in 10 secs. ', err);
-            setTimeout(tryMongoConnection, 3000)
+            clearTimeout(mongoTimeout);
+            mongoTimeout = setTimeout(tryMongoConnection, 3000)
         } else {
-            
+            logger.info('Connected to mongo queuerunner DB');
+
             immediate = new Immediate(db, nconf, logger).run();
             planned   = new Planned(db, nconf, logger).run();
             history   = new History(db, nconf, logger).run();
             watchdog  = new Watchdog(db, nconf, logger).run(immediate);
-            gui       = new Gui(db, nconf, logger, {
-                            immediate: immediate,
-                            planned:   planned,
-                            history:   history
-                        }).run();
+            if (nconf.get('gui:enable')) {
+                gui       = new Gui(db, nconf, logger, {
+                    immediate: immediate,
+                    planned:   planned,
+                    history:   history
+                }).run();
+            }
         }
     });
-}())
+}
+tryMongoConnection();
+
+logger.on('logging', function (transport, level, msg, meta) {
+    if (level == 'error' && meta.message && meta.message == 'topology was destroyed') {
+        logger.warn('MONGO TOPOLOGY DESTRUCTION DETECTED - stopping queues and reconnecting mongo');
+
+        if (nconf.get('gui:enable')) {
+            gui.stop();
+        }
+
+        planned.stop();
+        history.stop();
+        watchdog.stop();
+        immediate.stop(function(){
+            logger.warn('IMMEDIATE: instance with broken mongo just stopped');
+        });
+
+        tryMongoConnection();
+    }
+});
 
 // Graceful restart handler
 process.on( "SIGABRT", function() {
     var timeout = nconf.get('gracefulShutdownTimeout');
     logger.warn('SHUTDOWN: Graceful shutdown request detected. Stop queues and wait for '+timeout/1000+' seconds.');
 
-    gui.stop();
+    if (nconf.get('gui:enable')) {
+        gui.stop();
+    }
+
     planned.stop();
     history.stop();
     watchdog.stop();
