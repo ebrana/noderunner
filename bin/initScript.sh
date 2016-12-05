3c89c53946c99db434242ad58aa77a3d79706453
Jan Dvorak <dvorak@ebrana.cz>
2016-12-05 09:27:03 +0100

    run as another user than root

--- bin/initScript.sh
+++ bin/initScript.sh
@@ -2,33 +2,41 @@
 #
 # Note runlevel 2345, 86 is the Start order and 85 is the Stop order
 #
-# chkconfig: 2345 86 85
+# chkconfig: 2345 86 85 
 # description: Node.js daemon service for background running jobs from queuerunner MongoDB queue.
 
 export PATH=$PATH:/usr/local/bin
 export NODE_PATH=$NODE_PATH:/usr/local/lib/node_modules
 
+export NR_HOME=/home/noderunner
+export NR_LOGS=/var/log/noderunner
+export NR_USER=nginx
+export NR_GROUP=nginx
+
 case "$1" in
   start)
-  cd $(dirname `readlink -f $0 || realpath $0`)
-  cd ..
-  mkdir -p /var/log/noderunner
-  forever stop noderunner.js > /dev/null 2>&1
-  forever start -a -p . --killSignal=SIGABRT --minUptime 1000 --spinSleepTime 100 -e /var/log/noderunner/error.log -l /var/log/noderunner/forever.log --pidFile ./forever.pid noderunner.js
+  cd $NR_HOME
+
+  mkdir -p $NR_LOGS
+  chown -R $NR_USER:$NR_GROUP $NR_LOGS
+  sudo HOME=$NR_HOME -u $NR_USER -g $NR_GROUP forever stop -p $NR_HOME noderunner.js > /dev/null 2>&1
+  sudo HOME=$NR_HOME -u $NR_USER -g $NR_GROUP forever start -a -p . --killSignal=SIGABRT --minUptime 1000 --spinSleepTime 100 -e $NR_LOGS/error.log -l $NR_LOGS/forever.log --pidFile ./forever.pid noderunner.js
+  ;;
+status)
+  cd $NR_HOME
+  sudo HOME=$NR_HOME -u $NR_USER -g $NR_GROUP forever list
   ;;
 stop)
-  cd $(dirname `readlink -f $0 || realpath $0`)
-  cd ..
-  exec forever stop noderunner.js
+  cd $NR_HOME
+  sudo HOME=$NR_HOME -u $NR_USER -g $NR_GROUP forever stop noderunner.js
   ;;
 restart)
-  cd $(dirname `readlink -f $0 || realpath $0`)
-  cd ..
-  exec forever restart noderunner.js
+  cd $NR_HOME
+  sudo HOME=$NR_HOME -u $NR_USER -g $NR_GROUP forever restart noderunner.js
   ;;
 log)
-  cd $(dirname `readlink -f $0 || realpath $0`)
-  exec ./log.sh
+  cd $NR_HOME/bin
+  ./log.sh
   ;;  
 *)
   echo "Usage: /etc/init.d/noderunner {start|stop}"
