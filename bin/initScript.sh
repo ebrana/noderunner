d8a32e843a8bf87f894aeb256cfacf553feb6cdd
Jan Dvorak <dvorak@ebrana.cz>
2016-12-20 10:31:29 +0100

    automatic creation of forever pids folder in initscript

--- bin/initScript.sh
+++ bin/initScript.sh
@@ -18,7 +18,9 @@ case "$1" in
   cd $NR_HOME
 
   mkdir -p $NR_LOGS
+  mkdir -p $NR_HOME/.forever/pids
   chown -R $NR_USER:$NR_GROUP $NR_LOGS
+  chown -R $NR_USER:$NR_GROUP $NR_HOME/.forever/pids
   sudo HOME=$NR_HOME -u $NR_USER -g $NR_GROUP forever stop -p $NR_HOME noderunner.js > /dev/null 2>&1
   sudo HOME=$NR_HOME -u $NR_USER -g $NR_GROUP forever start -a -p . --killSignal=SIGABRT --minUptime 1000 --spinSleepTime 100 -e $NR_LOGS/error.log -l $NR_LOGS/forever.log --pidFile ./forever.pid noderunner.js
   ;;
