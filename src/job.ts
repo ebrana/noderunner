f1892b78588a79508c2faf4e8cdc92a1d7fe4f31
Martin Chudoba <martin.chudoba@seznam.cz>
2021-03-18 17:54:17 +0100

    revert join command arguments

--- src/job.ts
+++ src/job.ts
@@ -74,7 +74,7 @@ export default class Job {
 
         const spawn = require('child_process').spawn
         try {
-          const child = spawn(command[0], command[1].join(' '))
+          const child = spawn(command[0], command[1])
 
           this._save({
             pid: child.pid
